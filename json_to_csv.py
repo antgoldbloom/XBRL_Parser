fe55d0b91463bdcada820f60a2e91b36c3b3890e
Anthony Goldbloom <goldbloom@google.com>
2020-06-22 08:20:13 -0700

    added json_to_csv to this repo

---
+++ json_to_csv.py
@@ -0,0 +1,101 @@
+import json
+import numpy as np
+import pandas as pd
+import os
+
+json_path = '../data/json/'
+csv_path = '../data/csv/'
+
+ticker = 'AMZN'
+document_end_date = '2020-03-31'
+statement_name = 'ConsolidatedStatementsOfOperations'
+
+with open(f"{json_path}{ticker}.json", 'r') as stock_json:
+    stock_dict = json.loads(stock_json.read())
+
+stock_dict_with_ded = stock_dict[document_end_date]
+
+top_level_xlink_from = [] 
+
+
+#idetify top metric_label
+for metric in stock_dict_with_ded[statement_name]['metrics']:
+    metric_dict_slice = stock_dict_with_ded[statement_name]['metrics'][metric] 
+    if ('prearc_xlink:from' not in metric_dict_slice) and (metric[-4:] != '_YTD'): 
+        top_level_xlink_from.append(stock_dict_with_ded[statement_name]['metrics'][metric]['prearc_xlink:to'])
+
+
+def create_tmp_stock_dict(stock_dict_with_ded_statement_metric,metric):
+    tmp_stock_dict = dict()
+    tmp_stock_dict['metric'] = metric 
+    for key in dict_key_list:
+        if key in stock_dict_with_ded_statement_metric: 
+            tmp_stock_dict[key] = stock_dict_with_ded_statement_metric[key]
+    return tmp_stock_dict
+
+
+dict_key_list = ['label','prearc_order', 'prearc_xlink:from','prearc_xlink:to','value']
+stock_list_dict = dict()
+
+#pull out label chain for each numeric metric into dict
+for metric in stock_dict_with_ded[statement_name]['metrics']:
+    if 'value' in stock_dict_with_ded[statement_name]['metrics'][metric] and (metric[-4:] != '_YTD'): 
+
+        tmp_stock_dict_list = []
+        tmp_stock_dict = create_tmp_stock_dict(stock_dict_with_ded[statement_name]['metrics'][metric],metric)
+        tmp_stock_dict_list.append(tmp_stock_dict)
+
+        xlink_lower_metric = stock_dict_with_ded[statement_name]['metrics'][metric]['prearc_xlink:from']
+
+        xlink_lower_metric_match = None 
+
+        while (xlink_lower_metric_match not in top_level_xlink_from):
+            for metric_lower in stock_dict_with_ded[statement_name]['metrics']:  
+                if metric_lower[-4:] != '_YTD': #need this while I haven't addressed the YTD issue
+                    xlink_lower_metric_match = stock_dict_with_ded[statement_name]['metrics'][metric_lower]['prearc_xlink:to'] 
+                    if (xlink_lower_metric_match == xlink_lower_metric) and (xlink_lower_metric_match not in top_level_xlink_from):
+
+                        tmp_stock_dict = create_tmp_stock_dict(stock_dict_with_ded[statement_name]['metrics'][metric_lower],metric_lower)
+                        tmp_stock_dict_list.append(tmp_stock_dict)
+
+                        xlink_lower_metric = stock_dict_with_ded[statement_name]['metrics'][metric_lower]['prearc_xlink:from']
+
+                    elif (xlink_lower_metric_match == xlink_lower_metric) and (xlink_lower_metric_match in top_level_xlink_from):
+                        
+                        tmp_stock_dict = create_tmp_stock_dict(stock_dict_with_ded[statement_name]['metrics'][metric_lower],metric_lower)
+                        tmp_stock_dict_list.append(tmp_stock_dict)
+
+                        stock_list_dict[metric] = tmp_stock_dict_list 
+                        break
+
+
+
+
+####THE LIST IS CURRENT CORRECT ORDERED BY DEFAULT. CASN USE THIS IF THAT CHANGES
+#reorder_list = []
+#LEARN defaultdict in Python. That looks more flexible 
+#for metric in stock_list_dict:
+#    order_tmp = []
+#    for i in range(len(stock_list_dict[metric])-2,-1,-1):
+#        order_tmp.append(int(stock_list_dict[metric][i]['prearc_order'])) 
+#    reorder_list.append(order_tmp)
+
+#TO DO MAP REORDERING
+#print(sorted(reorder_list,reverse=False))
+
+metric_list = []
+for row in stock_list_dict:
+    metric_list.append(row)
+
+df_statement = pd.DataFrame(index=metric_list)  
+
+
+for metric in stock_list_dict:
+    for metric_date in stock_list_dict[metric][0]['value']:
+        df_statement.loc[df_statement.index==metric,metric_date] =  stock_list_dict[metric][0]['value'][metric_date]
+    
+    
+df_statement = df_statement.iloc[:, ::-1]
+
+
+print(df_statement)
