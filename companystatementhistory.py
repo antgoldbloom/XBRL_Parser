f4842d8b68818dc330c88a996b2ba003d7bfafbb
Anthony Goldbloom <goldbloom@google.com>
2020-07-13 12:47:31 -0700

    moved by company statement iteration inside class

---
+++ companystatementhistory.py
@@ -0,0 +1,380 @@
+
+import os
+import pandas as pd
+import numpy as np
+import re 
+import shutil
+from pathlib import Path
+from datetime import datetime
+from time import time
+
+
+import logging
+import string
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+stopwords = stopwords.words('english')
+
+class CompanyStatementHistory:
+
+
+    def __init__(self,ticker,csv_path,data_path,update_only=True):
+
+        #initialize key variables
+        self.ticker = ticker 
+        self.csv_path = csv_path
+        self.log_path = f"{data_path}logs/"
+        self.timeseries_path = f"{data_path}timeseries/"
+        self.latest_statement_date_type()
+
+        #configure logging
+        logfilename = f"{self.log_path}{self.ticker}_csv_to_timeseries_{datetime.now().strftime('%Y_%m_%d__%H_%M')}.log"
+        logging.basicConfig(filename=logfilename, filemode='w', format='%(levelname)s - %(message)s',level=logging.INFO)
+
+        #delete directory is we're doing a completely fresh pull
+        if update_only==False:
+            shutil.rmtree(f"{self.timeseries_path}{ticker}", ignore_errors=True, onerror=None)  #remove if exists 
+            Path(f"{self.timeseries_path}{ticker}").mkdir(parents=True, exist_ok=True)
+
+        #loop through each statement
+        overall_start_time = time()
+        for dirname, _, filenames in os.walk(f"{csv_path}{ticker}/{ self.latest_statement_date_type}"):
+            for statement in filenames:
+                self.create_statement_time_series(statement,logging)
+        time_taken = f"Total: time: {time() - overall_start_time}"
+        self.print_and_log(logging,time_taken)
+
+
+
+    def create_statement_time_series(self,statement,logging):
+        #self.statement = statement
+        #self.load_statements_into_dict
+        statement_dict = self.load_statements_into_dict(statement,logging) 
+        list_statement_dates = sorted(list(statement_dict.keys()),reverse=True)
+        timeseries_df = self.populate_timeseries(statement,statement_dict,list_statement_dates,logging) 
+        self.save_file(statement,timeseries_df,logging)
+
+    def populate_timeseries(self,statement,statement_dict,list_statement_dates,logging):
+
+        self.print_and_log(logging,statement)
+
+        timeseries_df = self.populate_timeseries_df(statement_dict,list_statement_dates,logging) 
+        timeseries_df = self.clean_up_timeseries_df(statement_dict,list_statement_dates,timeseries_df,logging)
+        self.check_dataframe(timeseries_df,logging)
+        return timeseries_df
+
+
+
+    def latest_statement_date_type(self):
+        ded_dict = {}
+        for dirname in os.listdir(f"{self.csv_path}{self.ticker}"):
+            ded_dict[dirname[:10]] = dirname 
+        
+        self.latest_statement_date_type = ded_dict[max(ded_dict.keys())]
+
+
+    def load_statements_into_dict(self,statement,logging):
+
+        statement_dict = {}
+        statement_dict[self.latest_statement_date_type] = pd.read_csv(os.path.join(f"{csv_path}{ticker}/{self.latest_statement_date_type}",statement),index_col=[0])
+
+        missing_list = []
+        for dirname, _, filenames in os.walk(f"{csv_path}{ticker}"):
+            missing_list.append(dirname)
+            max_overlap = 0
+            for filename in filenames:
+                if (filename[-4:] == '.csv'): 
+
+                    csim = self.calculate_csim([statement, filename])
+                    if csim == 1:
+                        most_likely_statement_match = filename 
+                        max_overlap = 1
+                        break #if filenames map perfectly don't even bother looking for column overlap
+                    elif csim > 0.3:
+        
+                        comp_df = pd.read_csv(os.path.join(dirname,filename),index_col=[0])
+                        overlap_percentage = len(set(statement_dict[self.latest_statement_date_type].index).intersection(comp_df.index))/len(statement_dict[self.latest_statement_date_type])  
+                    
+                        if overlap_percentage > max_overlap:
+                            max_overlap = overlap_percentage
+                            most_likely_statement_match = filename
+
+            if max_overlap > 0.1: #making sure there's some non trivial overlap
+                full_path = os.path.join(dirname,most_likely_statement_match)
+                ds = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2} \(10-[Q|K]\)',full_path)[0]
+                statement_dict[ds] = pd.read_csv(full_path,index_col=[0])
+                missing_list.remove(dirname)
+
+        missing_list = np.unique(missing_list)
+        self.log_missing(missing_list,statement,csv_path, ticker,logging)
+
+        return statement_dict
+
+    def clean_string(self,text):
+
+        text = text[:-4] 
+        text = ''.join([word for word in text if word not in string.punctuation])
+        text = text.lower()
+        text = ' '.join([word for word in text.split() if word not in stopwords])
+        return text
+
+    def cosine_sim_vectors(self,vec1,vec2):
+        vec1 = vec1.reshape(1,-1)
+        vec2 = vec2.reshape(1,-1)
+        return cosine_similarity(vec1,vec2)[0][0]
+
+    def calculate_csim(self,statement_names):
+        cleaned_statement_names = list(map(self.clean_string,statement_names))
+        vectorizer = CountVectorizer().fit_transform(cleaned_statement_names)
+        statement_name_vectors = vectorizer.toarray()
+        csim = self.cosine_sim_vectors(statement_name_vectors[0],statement_name_vectors[1]) 
+        return csim
+
+    def log_missing(self,missing_list,statement,csv_path,ticker,logging):
+            
+        missing_list = list(missing_list)
+        missing_list.remove(f"{csv_path}{ticker}")
+
+        for dirname in missing_list:
+            warning_message = f"{statement} equivalent not found in {dirname}"
+            print(warning_message) 
+            logging.warning(warning_message)  
+
+
+    def populate_timeseries_df(self,statement_dict,list_statement_dates,logging):
+
+        list_10k = []
+
+        for ds in list_statement_dates:
+            
+            date_cols = [date_col for date_col in statement_dict[ds].columns if re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}',date_col)]
+            tmp_df = statement_dict[ds][date_cols]
+
+            if ds[12:16] == '10-K':
+                list_10k.append(date_cols)
+
+            if ds == list_statement_dates[0]:
+                timeseries_df = tmp_df
+            else:
+                new_columns = tmp_df.columns.difference(timeseries_df.columns)
+                timeseries_df = timeseries_df.merge(tmp_df[new_columns],left_index=True,right_index=True,how='outer')
+                timeseries_df.loc[timeseries_df.index.isin(tmp_df.index),timeseries_df.columns.isin(tmp_df)] = tmp_df
+
+        timeseries_df = self.adjust_for_tag_changes_and_10k(list_10k,statement_dict,timeseries_df,logging)
+
+        return timeseries_df
+
+
+
+    def adjust_for_10k(self,list_10k,statement_dict,timeseries_df,tag_map):
+        list_10k = sorted(np.unique(np.concatenate(list_10k).flat),reverse=True)
+        timeseries_df = timeseries_df.reindex(sorted(timeseries_df.columns,reverse=True),axis=1)
+
+        for item_10k in list_10k:
+            statement_dict_key = f"{item_10k} (10-K)"
+
+            if statement_dict_key in statement_dict:
+                tmp_df = statement_dict[statement_dict_key]
+
+                tag_map_tmp_df_intersection = set(tag_map.keys()).intersection(tmp_df.index)
+                if len(tag_map_tmp_df_intersection) > 0:
+
+                    for metric_match in tag_map_tmp_df_intersection: 
+                        for metric in tag_map[metric_match]:
+                            tmp_df.loc[metric,:] = tmp_df.loc[metric_match,:] 
+                        tmp_df = tmp_df.drop(metric_match,axis=0)
+
+                tmp_df = tmp_df[tmp_df['period_type'] == 'ytd'] 
+                tmp_df = tmp_df[tmp_df.index.isin(timeseries_df.index)]
+
+                for date_col in [date_col for date_col in statement_dict[statement_dict_key].columns if re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}',date_col)]: 
+                    if date_col in list_10k:
+                        date_col_10k_index = (list(timeseries_df.columns).index(date_col))
+
+                        sequential_quarters = list(timeseries_df.columns)[date_col_10k_index:(date_col_10k_index+4)]
+                        sequential_quarters_count = self.count_sequential_quarters(sequential_quarters)
+                        if sequential_quarters_count == 3:
+                            not_nan_bool = timeseries_df.loc[tmp_df.index,sequential_quarters].notna().all(axis=1)
+                            not_nan_rows = tmp_df[not_nan_bool].index 
+                            is_nan_bool = timeseries_df.loc[tmp_df.index,sequential_quarters].isna().any(axis=1)
+                            is_nan_rows = tmp_df[is_nan_bool].index 
+                            timeseries_df.loc[not_nan_rows,date_col] = tmp_df.loc[not_nan_rows,date_col] - timeseries_df.loc[not_nan_rows,list(timeseries_df.columns)[date_col_10k_index+1:date_col_10k_index+4]].sum(axis=1)  
+                            timeseries_df.loc[is_nan_rows,date_col] = None 
+                        else:
+                            timeseries_df.loc[tmp_df.index,date_col] = None 
+
+        return timeseries_df
+
+
+    def adjust_for_tag_changes_and_10k(self,list_10k,statement_dict,timeseries_df,logging): 
+
+        tag_map = {} 
+        
+        tags_from_latest_statement = timeseries_df[(timeseries_df.isna().any(axis=1) & timeseries_df[timeseries_df.columns.max()].notna())]
+        for metric in tags_from_latest_statement.index:
+            for metric_match in timeseries_df[~timeseries_df.index.isin(tags_from_latest_statement.index)].index:
+
+                non_nan_columns = timeseries_df.loc[[metric,metric_match],:].notna().all(axis=0)
+                agreement_series = timeseries_df.loc[metric,non_nan_columns]==timeseries_df.loc[metric_match,non_nan_columns]
+
+                if (agreement_series.all() == True) and len(agreement_series > 1):
+                        timeseries_df.loc[metric,timeseries_df.columns[timeseries_df[timeseries_df.index == metric].isna().iloc[0].to_list()]] = timeseries_df.loc[metric_match,timeseries_df.columns[timeseries_df[timeseries_df.index == metric].isna().iloc[0].to_list()]] 
+                        if metric_match not in tag_map:
+                            tag_map[metric_match] = [] 
+                        tag_map[metric_match].append(metric) 
+
+        for metric in np.unique(tag_map.keys()):
+            timeseries_df = timeseries_df.drop(metric,axis=0)
+
+        if len(list_10k) > 0:
+            timeseries_df = self.adjust_for_10k(list_10k, statement_dict,timeseries_df,tag_map)
+
+        return timeseries_df
+
+
+
+
+    def clean_up_timeseries_df(self,statement_dict,list_statement_dates,timeseries_df,logging):
+        latest_filing_mask = timeseries_df.index.isin(statement_dict[list_statement_dates[0]].index)
+
+        timeseries_df_tmp = timeseries_df.loc[latest_filing_mask,:]
+        timeseries_df_tmp = timeseries_df_tmp.reindex(statement_dict[list_statement_dates[0]].index)
+        timeseries_df_tmp = timeseries_df_tmp.append(timeseries_df.loc[~latest_filing_mask,:])
+
+        timeseries_df_tmp = timeseries_df_tmp.loc[~timeseries_df_tmp.isna().all(axis=1),:]
+
+        timeseries_df = timeseries_df_tmp
+
+        return timeseries_df
+
+    def count_sequential_quarters(self,quarter_list,log_missing=False,logging=None):
+        sequential_quarters = 0
+        for i in range(1,len(quarter_list)):
+            diff = datetime.strptime(quarter_list[i-1],'%Y-%m-%d') - datetime.strptime(quarter_list[i],'%Y-%m-%d')
+            if diff.days > 85 and diff.days < 95:
+                sequential_quarters +=1
+            else:
+                if log_missing:
+                    message = f"Missing quarter between {quarter_list[i-1]} and {quarter_list[i]}"
+                    self.print_and_log(logging, message, True) 
+                else:
+                    break   
+        return sequential_quarters 
+
+    def check_dataframe(self,timeseries_df,logging):
+
+
+        expected_columns = round((datetime.strptime(timeseries_df.columns.max(), "%Y-%m-%d")-datetime.strptime(timeseries_df.columns.min(), "%Y-%m-%d")).days/(365/4))+1
+        actual_columns = len(timeseries_df.columns)
+        if (expected_columns - actual_columns) >= 0:  
+            message = f'Discontinuities estimate: {expected_columns-actual_columns}'
+
+        sequential_quarters = self.count_sequential_quarters(timeseries_df.columns, True, logging)
+        message = f"Estimate of sequential quarters: {sequential_quarters}"
+        self.print_and_log(logging,message)
+
+        message = f"Number of periods: {actual_columns}" 
+        self.print_and_log(logging,message)
+
+        message = f"Number of metrics: {len(timeseries_df)}" 
+        self.print_and_log(logging,message)
+
+        na_percentage = round(100*(len(timeseries_df)-timeseries_df.count()).sum()/(len(timeseries_df)*len(timeseries_df.columns)),2)
+        message = f"NA percentage:  {na_percentage}"
+        self.print_and_log(logging,message)
+
+    def statement_file_path_and_name(self,statement,logging):
+        dash_list = [m.start() for m in re.finditer('-', statement)]
+        if len(dash_list) >= 2: 
+            filetype = statement[dash_list[0]+2:dash_list[1]-1]
+            statement_name = statement[dash_list[1]+2:]
+        else:
+            message = f'Statement type could not be infered for {statement}'
+            self.print_and_log(message)
+            statement_name = statement 
+            filetype = 'Other'
+
+        return [f"{self.timeseries_path}{self.ticker}/{filetype}/",statement_name]
+
+    def save_file(self,statement,timeseries_df,logging):
+
+        statement_folder,statement_name = self.statement_file_path_and_name(statement,logging)
+        Path(statement_folder).mkdir(parents=True, exist_ok=True)
+
+        timeseries_df.to_csv(f"{statement_folder}{statement_name}") 
+
+        
+
+    def print_and_log(self,logging,message,warning=False):
+        print(message)
+        if warning:
+            logging.warning(message)
+        else:
+            logging.info(message)
+
+
+#### RUN CODE
+
+
+csv_path = '../data/csv/'
+data_path = '../data/'
+ticker = 'ZM'
+ZM_sh = CompanyStatementHistory(ticker,csv_path,data_path,False)
+print(ZM_sh.latest_statement_date_type)
+  
+
+
+#ticker_list = next(os.walk(csv_path))[1]
+#ticker_list = ['ZM'] 
+
+
+#for ticker in ticker_list:
+
+#logfilename = f"{log_path}csv_to_timeseries_{datetime.now().strftime('%Y_%m_%d__%H_%M')}.log"
+#logging.basicConfig(filename=logfilename, filemode='w', format='%(levelname)s - %(message)s',level=logging.INFO)
+#self.statement_dict = pick_latest_statement(csv_path,ticker)
+
+#print_and_log(logging,ticker)
+
+"""
+
+def add_labels_to_timeseries(self,self.timeseries_df,self.statement_dict,self.list_statement_dates):
+
+        for ds in self.list_statement_dates:
+
+            label_list = [l for l in self.statement_dict[ds].columns if 'label' in l .lower()]
+
+            for label in label_list:
+                if label not in self.timeseries_df.columns:
+                    self.timeseries_df[label] = None
+
+            if ds ==self.list_statement_dates[0]:
+                self.timeseries_df.loc[ self.statement_dict[ds].index,label_list] = self.statement_dict[ds].loc[:,label_list]
+            else:
+                labels_to_find_index = self.statement_dict[ds][self.statement_dict[ds].index.isin(labels_to_find_list)].index
+                self.timeseries_df.loc[labels_to_find_index,label_list] = self.statement_dict[ds].loc[labels_to_find_index,label_list] 
+
+            label_list = [l for l in self.timeseries_df.columns if 'label' in l .lower()] 
+
+            if self.timeseries_df[label_list].notna().any(axis=1).all() == False: #if any labels are still NA
+                labels_to_find_list = self.timeseries_df[self.timeseries_df[label_list].notna().any(axis=1) == False].index
+            else:
+                break
+
+        if 'label' not in self.timeseries_df.columns:
+            self.timeseries_df['label'] = None
+
+        for label in label_list:
+            self.timeseries_df.loc[self.timeseries_df['label'].isna(),'label'] = self.timeseries_df.loc[self.timeseries_df['label'].isna(),label]  
+
+        self.timeseries_df['xbrl_tag'] = self.timeseries_df.index 
+        self.timeseries_df = self.timeseries_df.set_index(['label','xbrl_tag']) 
+        
+        for label in [l for l in self.timeseries_df.columns if 'label' in l .lower()]: 
+            self.timeseries_df = self.timeseries_df.drop(label,axis=1)
+        
+        return self.timeseries_df 
+        
+
+"""
